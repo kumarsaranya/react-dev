cbbfd580eab1b46fa2b3dd5687a9a4cb528adfc8
saranya <kumarsaranya10494@gmail.com>
2025-06-10 06:48:37 +0530

    Food order app

--- App.js
+++ App.js
@@ -1,26 +1,66 @@
-/*
-<div parent>
-  <div child2>
-    <h1>
-    <h1>
-  </div>
- <div child2>
-  <h1>
-  <h1>
-  </div>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HeaderComponent from "./src/HeaderComponent";
+import SearchComponent from "./src/SearchComponent";
+import FilterComponent from "./src/FilterComponent";
+import RestaurantContainer from "./src/RestaurantContainer";
+import FooterComponent from "./src/FooterComponent";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
+import AboutComponent from "./src/AboutComponent";
+import ContactComponent from "./src/ContactComponent";
+import CartComponent from "./src/CartComponent";
+import ErrorComponent from "./src/ErrorComponent";
+import RestaurantMenu from "./src/RestaurantMenu";
 
-  </div>
-*/
+const reactJSX = <h1 className="heading"> Heading1 from JSX</h1>;
 
-const config = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "header1" }, "heading 1"),
-    React.createElement("h2", { id: "header2" }, "Header 2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "header1" }, "heading 1"),
-    React.createElement("h2", { id: "header2" }, "Header 2"),
-  ]),
+const AppComponent = () => {
+  return (
+    <div>
+      <HeaderComponent />
+      <Outlet />
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppComponent />,
+    children: [
+      {
+        path: "/",
+        element: <RestaurantContainer />,
+      },
+      {
+        path: "/about",
+        element: <AboutComponent />,
+      },
+      ,
+      {
+        path: "/cart",
+        element: <CartComponent />,
+      },
+      {
+        path: "/contact",
+        element: <ContactComponent />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <ErrorComponent />,
+  },
+  {
+    path: "/about",
+    element: <AboutComponent />,
+  },
+  {
+    path: "/contact",
+    element: <ContactComponent />,
+  },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(config);
+
+root.render(<RouterProvider router={appRouter} />);
